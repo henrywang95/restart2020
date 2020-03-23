628d7d560e001b18974ee6885e6004dfca0a4293
henrywang95 <henrywang1995@gmail.com>
2020-03-23 09:26:57 -0400

    add css

--- index.js
+++ index.js
@@ -22,28 +22,26 @@ app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded({extended:true})); // to support URL-encoded bodies
 
 
+
 app.get("/", (request, response) => {
     response.sendFile("index.html");
 });
 
 // Show all my submissions
-app.get("/2", (req, res) => {
+app.get("/yes", (req, res) => {
     res.sendFile(path.join(__dirname + '/public/yes.html'))
 })
 
-app.get("/3", function (req, res) {
+app.get("/no", function (req, res) {
     res.sendFile(path.join(__dirname + '/public/no.html'))
   })
 
-// app.get("/4", function (req, res) {
-//     res.sendFile(path.join(__dirname + '/public/submission.html'))
-// })
 
-app.get("/post", (req, res) => {
+app.get("/hope", (req, res) => {
     res.sendFile(path.join(__dirname + '/public/submission.html'))
-})
+});
 
-app.get("/api", (req, res) => {    
+app.get("/hope", (req, res) => {    
     db.find({}, function (err, docs) {
         if(err){
             return err;
@@ -51,22 +49,36 @@ app.get("/api", (req, res) => {
         res.json(docs);
     });
 });
+app.post("/hope", (req, res) => {
+        res.sendFile(path.join(__dirname + '/public/submission.html'));
+        // our unix timestamp
+        const unixTimeCreated = new Date().getTime();
+        // add our unix time as a "created" property and add it to our request.body
+        const newData = Object.assign({"created": unixTimeCreated}, req.body)
+        // add in our data object to our database using .insert()
+        db.insert(newData, (err, docs) =>{
+            if(err){
+                return err;
+            }
+            //res.json(docs);
+        });
+});
 
-app.post("/post", (req, res) => {
-    const unixTimeCreated = new Date().getTime();
-    let newpost = req.body.newhope;
+// app.post("/hope/api", (req, res) => {
+//     // our unix timestamp
+//     const unixTimeCreated = new Date().getTime();
+//     // add our unix time as a "created" property and add it to our request.body
+//     const newData = Object.assign({"created": unixTimeCreated}, req.body)
+//     // add in our data object to our database using .insert()
+//     db.insert(newData, (err, docs) =>{
+//         if(err){
+//             return err;
+//         }
+//         res.json(docs);
+//     });
 
-    // add our unix time as a "created" property and add it to our request.body
-    const newData = Object.assign({"created": unixTimeCreated}, req.body)
-    // add in our data object to our database using .insert()
-    db.insert(newData, (err, docs) =>{
-        if(err){
-            return err;
-        }
-        res.json(docs);
-    });
-})
-  
+// })
+
 
 http.createServer(app).listen(port, () =>{
         console.log(`see the magic at: http://localhost:${port}`)
--- public/sketch.js
+++ public/sketch.js
@@ -1,9 +1,3 @@
-// console.log("hello from index")
-let capture;
-let submitButton;
-let locationData;
-
-
 function setup(){
     createCanvas(200, 200).parent("#mySketch");
     capture = createCapture(VIDEO);
@@ -20,21 +14,11 @@ function setup(){
 
     
 }
-
 function handleSubmit(e){
     let output = {
         location: {},
         image: ''
     }
-    
-    output.location.lat = locationData.latitude
-    output.location.lon = locationData.longitude
-    
-    const last_img = get()
-    output.image = last_img.canvas.toDataURL()
-
-    console.log(last_img)
-
     const options = {
         method: 'POST',
         headers: {
@@ -47,22 +31,4 @@ function handleSubmit(e){
         // updateMyDots()
         console.log('success')
     })
-
-    // httpPost("/api", output, (result) =>{
-    //     // console.log(result);
-    //     console.log("success")
-    // });
-}
-
-function doThisOnLocation(position){
-    locationData = position
-    console.log(position.latitude)
-    console.log(position.longitude)
-    select("#lat").html( nfc(position.latitude, 4) )
-    select("#lon").html( nfc(position.longitude, 4))
-}
-
-function draw(){
-    background(220);
-    image(capture,width/2,height/2, width*1.3, height)
 }
